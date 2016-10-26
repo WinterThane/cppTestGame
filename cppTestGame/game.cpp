1875bd9407ecc89a52a3de5b3b55527f77c0aa8e
WinterThane <demonsaso@gmail.com>
2016-10-26 19:36:02 +0200

    FPS added
    fps timer added

--- cppTestGame/game.cpp
+++ cppTestGame/game.cpp
@@ -1,12 +1,20 @@
 #include "game.h"
 
 #include <conio.h>
+#include <Windows.h>
 #include <iostream>
+
+using namespace std;
+
+#define GAME_SPEED 33.33 // game speed - 30 FPS
 
 bool Game::run()
 {
 	char key = ' ';
+
+	startTime = timeGetTime();
 	frameCount = 0;
+	lastTime = 0;
 
 	while(key != 'q')
 	{
@@ -14,11 +22,11 @@ bool Game::run()
 		{		
 			timerUpdate();
 		}
-		std::cout << "You pressed: " << key << std::endl;
+		cout << "You pressed: " << key << endl;
 	}
 
-	std::cout << frameCount << std::endl;
-	std::cout <<  "End of the game" << key << std::endl;
+	cout << frameCount / ((timeGetTime() - startTime) / 1000) << " FPS " << endl;
+	cout <<  "End of the game " << endl;
 	system("pause");
 	return true;
 }
@@ -36,5 +44,12 @@ bool Game::getInput(char* c)
 
 void Game::timerUpdate()
 {
+	double currentTime = timeGetTime() - lastTime;
+
+	if (currentTime < GAME_SPEED)
+		return;
+
 	frameCount++;
+
+	lastTime = timeGetTime();
 }
